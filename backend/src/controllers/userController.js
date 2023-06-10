eca91f01e87e5cf7ddd308553c10b7d69b0f3e61
Samuel Victor Oliveira Lima <990905>
2023-06-09 21:48:59 -0300

    cria endpoint post

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,8 +1,28 @@
-const getAllFavUsers = (_req, res) => {
+const fs = require('fs');
+const path = require('path');
+
+const getAllUsers = (_req, res) => {
     const favUsersDB = require('../data/db.json')
     return res.status(200).json(favUsersDB)
 }
 
+const addUser = (req, res) => {
+    console.log("req body:\n" + req.body)
+    const newUser = req.body;
+    const dbPath = path.join(__dirname, '../data/db.json');
+    const favUsersDB = require(dbPath);
+  
+    // Adiciona o novo usuário à lista de usuários favoritos
+    favUsersDB.push(newUser);
+  
+    // Atualiza o arquivo db.json com a nova lista de usuários favoritos
+    fs.writeFileSync(dbPath, JSON.stringify(favUsersDB));
+  
+    // Retorna uma resposta de sucesso
+    res.status(201).json({ message: 'Usuário adicionado com sucesso' });
+}
+
 module.exports = {
-    getAllFavUsers,
+    getAllUsers,
+    addUser
 }
--- backend/src/router.js
+++ backend/src/router.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const usersController = require('./controllers/userController');
 
-router.get('/users', usersController.getAllFavUsers);
+router.get('/users', usersController.getAllUsers);
+
+router.post('/users', usersController.addUser);
 
 module.exports = router;
