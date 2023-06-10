34d8c2a0814d9fae509cc76dd719348b386bbcb1
Samuel Victor Oliveira Lima <990905>
2023-06-09 21:33:26 -0300

    cria frontend e component searchbar

---
+++ frontend/src/app/interfaces/GitHubUser.ts
@@ -0,0 +1,7 @@
+export default interface GitHubUser {
+    login: string;
+    name: string;
+    avatar_url: string;
+    html_url: string;
+    fav: boolean;
+  }
---
+++ frontend/src/app/searchbar/searchbar.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import GitHubUser from '../interfaces/GitHubUser';
+import { UserService } from '../services/user.service';
+
+@Component({
+  selector: 'app-searchbar',
+  templateUrl: './searchbar.component.html',
+  styleUrls: ['./searchbar.component.css']
+})
+export class SearchbarComponent implements OnInit {
+  username = '';
+  user: GitHubUser | null = null;
+  modal = false;
+
+  constructor(private userService: UserService) { }
+
+
+  ngOnInit(): void {
+  }
+
+  async findUser() {
+    try {
+      const response = await fetch(`https://api.github.com/users/${this.username}`);
+      if (response.ok) {
+        this.modal = true;
+        this.user = await response.json();
+      } else {
+        alert('nao achou o usuario')
+      }
+    } catch (error) {
+      alert('Não foi possivel buscar o usuario no momento.')
+    }
+  }
+
+  closeModal() {
+    this.modal = false;
+  }
+
+}
